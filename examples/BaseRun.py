6b71b12ded04421bb4a6594efdef516f9f383b91
Dou <dl3182@nyu.edu>
2018-05-21 15:48:04 -0400

    import packages

---
+++ examples/BaseRun.py
@@ -0,0 +1,12 @@
+
+import sys
+
+sys.path =["package","../package"]+sys.path
+
+from BaseReconstruct import *
+from BaseInfo import *
+from ReconstructG import *
+from ReconstructShep import *
+from writecube import *
+from Reanalysis import *
+from plots import *
