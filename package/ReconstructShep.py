2108518b22d57c9a532d2587e6de43acf5363fd1
Dou Liu <dl3182@nyu.edu>
2018-06-11 11:52:05 -0400

    Update ReconstructShep.py

--- package/ReconstructShep.py
+++ package/ReconstructShep.py
@@ -6,62 +6,67 @@ import numpy as np
 import time
 
 
-class ReconstructShep(BaseReconstruct):
+class ReconstructShep(object):
     def __init__(self, base=None, dimage=0.5):
+        #     def __init__(self, rssfile=None, dimage=0.5,nkernel=201,alpha=1,beta=1,waveindex=None):
+        #         BaseReconstruct.__init__(self,rssfile=rssfile, dimage=dimage,nkernel=nkernel,alpha=1,beta=1,waveindex=waveindex)
         if (base.dimage != dimage):
             print('Pixel size cannot match')
-            dimage = base.dimage
         self.__dict__ = base.__dict__.copy()
 
+        Shepard_cov = []
         if self.single:
-            Shepard, Shepard_2, Shep_cov, Shepard_ivar = self.Shepard_core(waveindex)
-            Shepard_cov = []
+            Shepard, Shepard_2, Shep_cov, Shepard_ivar, Shepard_cons = self.Shepard_core(self.waveindex)
+
             Shepard_cov.append(Shep_cov)
             Indicator = self.Indicate.copy()
         else:
             start_time = time.time()
 
             nWave = base.nWave
-            Indicator, Shepard, Shepard_2, Shepard_ivar = (np.zeros((nWave, self.nside, self.nside)) for i in range(4))
-            Shepard_cov = []
+            Indicator, Shepard, Shepard_2, Shepard_ivar, Shepard_cons = (np.zeros((nWave, self.nside, self.nside)) for i
+                                                                         in range(5))
 
             for iWave in self.range:
                 if (iWave % 1000 == 0):
                     print('Shepard wavelength channel', iWave)
-                Shepard[iWave], Shepard_2[iWave], Shep_cov, Shepard_ivar[iWave] = self.Shepard_core(iWave)
+                Shepard[iWave], Shepard_2[iWave], Shep_cov, Shepard_ivar[iWave], Shepard_cons[
+                    iWave] = self.Shepard_core(iWave)
                 Shepard_cov.append(Shep_cov)
                 Indicator[iWave] = self.Indicate.copy()
 
             stop_time = time.time()
             print("Shepard interation Time = %.2f" % (stop_time - start_time))
 
-        # result assignment
         self.IMGresult = Shepard
         self.PSFresult = Shepard_2
         self.cov = Shepard_cov
         self.ivariance = Shepard_ivar
         self.Indicator = Indicator
-        if (len(self.range) == self.nWave and (self.single == False)):
+
+        self.PSF_cons = Shepard_cons
+
+        if ((len(self.range) == self.nWave) and (self.waveindex == None)):
             self.analysis()
 
-    # main part for Shepard's
     def Shepard_core(self, iWave):
         Shep_cov, Shepard_ivar = self.set_fit(ivar=self.ivar[:, iWave], xsample=self.xpos[:, iWave],
                                               ysample=self.ypos[:, iWave])
         Shepard = self.solve_Shepard(value=self.value[:, iWave])
         if self.single:
             Shepard_2 = self.solve_Shepard(value=self.value_PSF)
+            Shepard_cons = self.solve_Shepard(value=self.value_cons)
         else:
             Shepard_2 = self.solve_Shepard(value=self.value_PSF[iWave])
-        return (Shepard, Shepard_2, Shep_cov, Shepard_ivar)
+            Shepard_cons = self.solve_Shepard(value=self.value_cons[iWave])
+        return (Shepard, Shepard_2, Shep_cov, Shepard_ivar, Shepard_cons)
 
-    # weights
     def set_fit(self, ivar, xsample, ysample, shepard_sigma=0.7):
         nsample = len(xsample)
         dx = np.outer(xsample, np.ones(self.nimage)) - np.outer(np.ones(nsample), self.ximage)
         dy = np.outer(ysample, np.ones(self.nimage)) - np.outer(np.ones(nsample), self.yimage)
         dr = np.sqrt(dx ** 2 + dy ** 2)
-        w = np.transpose(np.matlib.repmat(ivar != 0, self.nside ** 2, 1)) * np.exp(- 0.5 * dr ** 2 / shepard_sigma ** 2)
+        w = np.transpose(np.matlib.repmat(ivar != 0, self.nside ** 2, 1))*np.exp(- 0.5 * dr ** 2 / shepard_sigma ** 2)
         ifit = np.where(dr > 1.6)
         w[ifit] = 0
 
@@ -70,14 +75,8 @@ class ReconstructShep(BaseReconstruct):
         wsum = w.sum(axis=0)
         Indicate = np.ones(self.nside * self.nside)
 
-        # ratio=30
-        # self.ifit= np.where(wsum > wsum.max() * ratio*1.e-2)[0]
-        #
-        # self.Indicate = np.ones(self.nside * self.nside)
-        # self.Indicate[self.ifit]=0
         self.Indicate = Indicate.reshape(self.nside, self.nside)
 
-        #         w = w[:, self.ifit]
         self.ww = np.zeros(w.shape)
         for i in range(self.nimage):
             if wsum[i] == 0:
@@ -86,12 +85,10 @@ class ReconstructShep(BaseReconstruct):
                 self.ww[:, i] = w[:, i] / wsum[i]
         self.wwT = self.ww.T
         Shep_cov = self.wwT.dot(self.ww) * self.conversion ** 2
-        Shep_cov_diag = np.diag(Shep_cov).reshape(self.nside, self.nside)
         Shep_ivar = 1 / (np.diag(Shep_cov).reshape(self.nside, self.nside) + 1E-50 * np.ones([self.nside, self.nside]))
         Shep_ivar[np.where(Shep_ivar > 1E30)] == 0
         return (Shep_cov, Shep_ivar)
 
-    # solve the reconstruction
     def solve_Shepard(self, value):
         Shep = (self.wwT.dot(value)).reshape(self.nside, self.nside) * self.conversion
         return Shep
