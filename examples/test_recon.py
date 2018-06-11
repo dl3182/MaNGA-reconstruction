8c8859ca00f002e86585ccc43cc854f43f43e887
Dou Liu <dl3182@nyu.edu>
2018-06-11 13:08:03 -0400

    Update test_recon.py

--- examples/test_recon.py
+++ examples/test_recon.py
@@ -4,24 +4,56 @@ import time
 from BaseRun import *
 
 
+# BaseRun.basekernel(plate,ifu,dimage=0.75,nkernel=201,waveindex=None,add_exposures=None,single_kernel=None)
+
+# parameters:
+# plate,ifu: the plate-ifu of MaNGA data
+# dimage: the pixel size of reconstruction
+# nkernel: the number of pixels when constructing the kernel on a finer grid
+# waveindex: the index of wavelength, None means the whole spectrum
+# add_exposures: means whether add extra exposures for a simulation
+#     1: add the central point for ditherings
+#     3: add offset ditherings
+#     4: add the averages of three exposures of ditherings
+# single_kernel:means whether we use kernel for single exposure to speed up the program, True means using.
+# return:
+#     a class which includes the information of the kernel
+# quantities:
+#    measurements:nFiber,nExp,wave,nWave,xpos,ypos,nimage,nsample,value,ivar
+#    regular grid: length,nside,waveindex,xi/yi
+#    kernel:kernelvalue,imagevalue,value_PSF
+# ---------------------------------------------------------------------------------------------------------------------
+# BaseRun.Reconstruction(plate=None,ifu=None,dimage=0.75,nkernel=201,waveindex=None,ratio=25,lam=0,single_kernel=None):
+
+# parameters:
+# plate,ifu: the plate-ifu of MaNGA data
+# dimage: the pixel size of reconstruction
+# nkernel: the number of pixels when constructing the kernel on a finer grid
+# waveindex: the index of wavelength, None means the whole spectrum
+# ratio:the criterion to select the pixels in the G's configuration
+# lam: the regularization coefficient for G's method
+# single_kernel:means whether we use kernel for single exposure to speed up the program, True means using.
+# return:
+#    two classes which includes the information of Shepard and G
+# quantities:
+# IMGresult: the true reconstruction with regard to real flux
+# PSFreulst: the simulation reconstruction with regard to a point source
+# PSF_cons: the simulation result with regard to a flat field
+# cov: the covariance of the reconstruction
+# ivariance: the inverse of the diagonal of the covariance
+# Indicator: the selected pixels for each wavelength
+# For G, the quantities also include
+#     F: the deconvolved result with regard to real flux
+#     F2: the deconvolved result with regard to a point source
+#     F_cons: the deconvolved result with regard to a flat field.
+
 
 start_time = time.time()
-plate = 7977
+plate=8551
+ifu=1902
+Shepard,G = Reconstruction(plate=plate,ifu=ifu,waveindex=2000)
 
-ifu = 701
-
-rssfile = getrss(plate=plate,ifu=ifu)
-
-print('kernel')
-base=BaseReconstruct(rssfile=rssfile,dimage=0.75,nkernel=151)
-# print('Shepard spectrum')
-# spectrumShep=ReconstructShep(base=base,dimage=0.5)
-print('G spectrum')
-spectrumG=ReconstructG(base=base,dimage=0.75,ratio=30)
-
-G_cube=write(rssfile, spectrumG, 'G2-'+str(plate)+'-'+str(ifu)+'-LOGCUBE')
-# Shep_cube=write(rssfile, spectrumShep, 'Shepard-'+str(plate)+'-'+str(ifu)+'-LOGCUBE')
-
+base=basekernel(plate=plate,ifu=ifu,add_exposures=4,single_kernel=10)
 
 stop_time= time.time()
 
